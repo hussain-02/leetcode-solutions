9f802663091549038ad4432a676151a3c1031dd2
MOHAMMED HUSSAIN F <hussainofficial236@gmail.com>
2026-03-19 09:51:04 +0530

    Time: 4 ms (99.94%), Space: 51 MB (92.58%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,23 +1,42 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
+
         if(k <= 1) return 0;
          int n = nums.length;
          int pro = 1;
          int cnt = 0;
+         int left = 0;
 
+         for(int r = 0 ; r < n ; r++){
+            pro *= nums[r];
 
-         for(int i = 0 ; i < n ; i++){
-            pro =1;
-            for(int j = i ; j >= 0 ; j--){
-                pro *= nums[j];
-
-                if(pro < k) cnt++;
-                else break;
+            while(pro >= k){
+                 pro /= nums[left];
+                 left++;
             }
-         }
 
-
+            cnt += (r - left + 1);
+         } 
          return cnt;
+
+        // if(k <= 1) return 0;
+        //  int n = nums.length;
+        //  int pro = 1;
+        //  int cnt = 0;
+
+
+        //  for(int i = 0 ; i < n ; i++){
+        //     pro =1;
+        //     for(int j = i ; j >= 0 ; j--){
+        //         pro *= nums[j];
+
+        //         if(pro < k) cnt++;
+        //         else break;
+        //     }
+        //  }
+
+
+        //  return cnt;
         
     }
 }

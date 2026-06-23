2a9c0099beb746e8a92f805c6cbb100033f2ff2f
MOHAMMED HUSSAIN F <hussainofficial236@gmail.com>
2026-06-23 10:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (86.85%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,19 +1,64 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return build(s).equals(build(t));
-    }
 
-    public String build(String str){
+        int i = s.length() - 1;
+        int j = t.length() - 1;
 
-        Stack<Character> ans = new Stack<>();
+        int skipS = 0;
+        int skipT = 0;
 
-        for(char ch : str.toCharArray()){
-            if(ch != '#') ans.push(ch);
-            else if(!ans.empty()){
-                ans.pop();
+        while(i >= 0 || j >= 0){
+            while(i >= 0){
+                if(s.charAt(i) == '#'){
+                    skipS++;
+                    i--;
+                }else if(skipS > 0){
+                    skipS--;
+                    i--;
+                }else{
+                    break;
+                }
             }
-        }
 
-        return String.valueOf(ans);
+                while( j >= 0){
+                    if(t.charAt(j) == '#'){
+                        skipT++;
+                        j--;
+                    }else if(skipT > 0){
+                        skipT--;
+                        j--;
+                    }else{
+                        break;
+                    }
+                }
+
+                if( i >= 0 && j >= 0){
+                    if(s.charAt(i) != t.charAt(j)) return false;
+                }else{
+                    if (i >=0 || j >= 0) return false;
+                }
+
+                i--;
+                j--;
+            }
+            return true;
+        //return build(s).equals(build(t));
+
+        }
     }
-}
+        
+
+    // public String build(String str){
+
+    //     Stack<Character> ans = new Stack<>();
+
+    //     for(char ch : str.toCharArray()){
+    //         if(ch != '#') ans.push(ch);
+    //         else if(!ans.empty()){
+    //             ans.pop();
+    //         }
+    //     }
+
+    //     return String.valueOf(ans);
+    // }
+// }
